e3c419b986f4f31151ac19558e9a042e6ddf58bb
saraliva4 <122287923+saraliva4@users.noreply.github.com>
2026-03-04 16:02:37 +0100

    Added elements for UX

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function generatePoem(response) {
+function displayPoem(response) {
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -7,18 +7,19 @@ function generatePoem(response) {
   });
 }
 
-function getAPI(event) {
+function generatePoem(event) {
   event.preventDefault();
   let topicInput = document.querySelector("#topic-input");
+  let poemElement = document.querySelector("#poem");
+  poemElement.innerHTML = `<div class="generating">Generating Danish poem about ${topicInput.value}⏳</div>`;
+
   let context = "You are a poet who writes short rhyming poems in Danish.";
   let prompt = `Please, write me a poem about ${topicInput.value}. It should be written in basic HTML and each line should be separated by </ br>. The answer should only be the poem and the title, and you should sign it with "SheCodes AI". Please put the title in an <h3> and put the signature in a <em> tag. Please, behave and don't include the quotes or the word HTML`;
   let apiKey = "02f8a7tad0e0efa9c2364cdececoab3a";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  axios.get(apiUrl).then(generatePoem);
+  axios.get(apiUrl).then(displayPoem);
 }
 
-let poem = document.querySelector("#poem");
-
 let submitButton = document.querySelector("#topic-submit");
-submitButton.addEventListener("click", getAPI);
+submitButton.addEventListener("click", generatePoem);
